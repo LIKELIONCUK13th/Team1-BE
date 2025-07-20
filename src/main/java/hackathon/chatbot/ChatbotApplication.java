8e78f6c894f97dd7e1449b2fcefa05c3e87d49d4
TaeRim0701 <xofla71@naver.com>
2025-07-20 23:36:02 +0900

    [fix] cors 오류 해결

--- src/main/java/hackathon/chatbot/ChatbotApplication.java
+++ src/main/java/hackathon/chatbot/ChatbotApplication.java
@@ -2,6 +2,9 @@ package hackathon.chatbot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ChatbotApplication {
@@ -10,4 +13,22 @@ public class ChatbotApplication {
         SpringApplication.run(ChatbotApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "http://localhost:8080",
+                                "https://routeez.netlify.app/"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .maxAge(3600);
+            }
+        };
+    }
 }
