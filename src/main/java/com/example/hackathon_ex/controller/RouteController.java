3e2d00c7e4c3d3791f5eae41e852bcc6ca7605a8
TaeRim0701 <xofla71@naver.com>
2025-07-20 23:56:50 +0900

    [fix] Kakao Map API 수정

--- src/main/java/com/example/hackathon_ex/controller/RouteController.java
+++ src/main/java/com/example/hackathon_ex/controller/RouteController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/map")
+@RequestMapping("/map")
 @RequiredArgsConstructor
 public class RouteController {
 
